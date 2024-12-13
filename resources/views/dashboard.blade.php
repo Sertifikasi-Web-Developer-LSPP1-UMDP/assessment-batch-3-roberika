67f6be7a6a4062a7776dddbc5c2548106712cf03
Robert Antonius <robert.antonius23@gmail.com>
2024-12-13 17:36:04 +0700

    Menampilkan gambar yang diupload

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -71,8 +71,8 @@
                 <div id="{{ $announcement->id }}" class="hidden target:block p-6 text-preset bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" >   
                     <h3 class="text-xl font-semibold">{{ $announcement->title }}</h3>
                     <br/>
-                    @if ($announcement->image)
-                        <img src="{{ asset('images/' . $announcement->image) }}" alt="Announcement Image" class="mt-4">
+                    @if ($announcement->image_url)
+                        <img src="{{ asset('img/announcements/' . $announcement->image_url) }}" alt="{{ $announcement->title }}" class="mt-4">
                         <br/>
                     @endif
                     <p class="text-gray-600">{{ $announcement->summary }}</p>
